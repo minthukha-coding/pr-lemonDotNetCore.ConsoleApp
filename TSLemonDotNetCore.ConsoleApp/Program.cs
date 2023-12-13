0194dd9076669f7990e0d79672b43d4867be4ed1
User <User@DESKTOP-JFDF37H>
2023-12-13 14:57:38 +0630

    Add project files.

---
+++ TSLemonDotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+{
+    DataSource = ".",
+    InitialCatalog = "TestLemonDotNetCore",
+    UserID = "sa",
+    Password = "sasasu"
+};
+
+SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+connection.Open();
+Console.WriteLine("Connection is opened");
+
+string query = @"SELECT [Blog_Id]
+      ,[Blog_Title]
+      ,[Blog_Author]
+      ,[Blog_Content]
+  FROM [dbo].[Tbl_Blog]";
+
+SqlCommand command = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);    
+
+connection.Close();
+Console.WriteLine("Connection is closed");
+
+Console.ReadKey();
